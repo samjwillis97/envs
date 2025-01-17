6673182f87bd139fa70347e00afa74e934ef78f8
samjwillis97 <sam@williscloud.org>
2025-01-17 13:09:27 +1100

    Adding Node 22

---
+++ node/22/flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        nativeBuildInputs = with pkgs; [ ];
+        buildInputs = with pkgs; [ ];
+        packages = with pkgs; [ nodejs_22 ];
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          inherit packages buildInputs nativeBuildInputs;
+          shellHook = ''
+            export PATH="$PATH:$PWD/node_modules/.bin"
+          '';
+        };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
